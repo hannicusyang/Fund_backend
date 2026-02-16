14e0462164dd7360b90c7bc45a874a0aa8b5a94e
hannicus-nas <hannicusNas@gmail.com>
2026-02-16 03:22:16 +0000

    fix: 使用0.0.0.0替代::以兼容NAS

--- config/env_config.py
+++ config/env_config.py
@@ -8,21 +8,21 @@ ENV = os.getenv('FLASK_ENV', 'development')
 ENV_CONFIGS = {
     'development': {
         'DEBUG': True,
-        'HOST': '::',  # 监听所有IPv4和IPv6地址
+        'HOST': '0.0.0.0',  # 监听所有IPv4地址
         'PORT': 5000,
         'FRONTEND_URL': 'http://localhost:5173',
         'DB_HOST': '10.60.134.151',
     },
     'test': {
         'DEBUG': True,
-        'HOST': '::',
+        'HOST': '0.0.0.0',
         'PORT': 5000,
         'FRONTEND_URL': 'http://test.example.com',
         'DB_HOST': '10.60.134.151',
     },
     'production': {
         'DEBUG': False,
-        'HOST': '::',
+        'HOST': '0.0.0.0',
         'PORT': 51717,
         'FRONTEND_URL': 'http://hannicusworld.asia:11717',
         'DB_HOST': '10.60.134.151',
